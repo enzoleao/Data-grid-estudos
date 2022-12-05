e303c5f5674a81f2c28cf4063c0f958e24d00a90
enzoleao <enzopinheiro6@gmail.com>
2022-12-05 13:49:25 -0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Table from './Table'
+function App() {
+  return (
+    <Table
+      id={1}
+      segundoNome="enzo"
+      primeironome="Leao"
+      idade={12}
+    />
+  )
+}
+
+export default App
---
+++ src/Table.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+export default function Table(props) {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {field: 'age',headerName: 'Age', width: 90,},
+  ];
+  
+  const rows = [
+    { id: `${props.id}`, lastName:`${props.primeiroNome}`, firstName: `${props.segundoNome}`, age: props.idade },
+   
+  ];
+  return (
+    <>
+    <div style={{width:'80%'}}>
+
+      <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+    </div>
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="id"
+            disabled
+            label="Email Address"
+            value={props.id}
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+    </>
+  )
+}
+
